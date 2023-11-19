5dbba6d24c00568e7bf2802f9b135b0c7c16933b
Developer Baboo <babooheerani@gmail.com>
2023-11-19 08:51:03 +0500

    added further functionality

---
+++ app/Livewire/Productlist.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Livewire;
+
+use App\Models\Product;
+use Livewire\Component;
+use App\Models\Shippingcart;
+
+class Productlist extends Component
+{
+    public function render()
+    {
+        $products = Product::get();
+        return view('livewire.productlist', ['products' => $products]);
+    }
+
+
+    public function addToCart($id){
+        // dd($id);
+
+
+        if(auth()->user()){
+            // Todo: add to  cart
+
+            $data = [
+                'user_id' => auth()->user()->id,
+                'product_id' => $id,
+            ];
+            Shippingcart::updateOrCreate($data);
+            session()->flash('success', 'Product added to cart successfully');
+        }else{
+            return redirect(route('login'));
+        }
+    }
+}
