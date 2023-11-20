8981cdd47d575e5f0b821b2409760b8381d6dbdd
Developer Baboo <babooheerani@gmail.com>
2023-11-20 21:41:45 +0500

    added further functionalities

--- app/Livewire/ShoppingCart.php
+++ app/Livewire/ShoppingCart.php
@@ -1,15 +1,23 @@
 <?php
 
+// Livewire component
 namespace App\Livewire;
 
 use Livewire\Component;
-use App\Models\Shippingcart as Cart;
+use Illuminate\Support\Facades\Auth;
+use App\Models\Shoppingcart as ShoppingCartModel;
 
 class ShoppingCart extends Component
 {
+    public $cartitems;
+
     public function render()
     {
+        $this->cartitems = ShoppingCartModel::with('product')
+            ->where('user_id', Auth::id())
+            ->get();
 
-        return view('livewire.shoppingcart');
+        return view('livewire.shoppingcart')->layout('layouts.app');
     }
 }
+
